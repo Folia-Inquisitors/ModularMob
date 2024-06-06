cb4ce8e653770d18660aebf776bb735ee1603dc6
hsgamer <huynhqtienvtag@gmail.com>
2024-06-06 11:54:54 +0700

    init

---
+++ src/main/java/me/hsgamer/modularmob/ModularMob.java
@@ -0,0 +1,17 @@
+package me.hsgamer.modularmob;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ModularMob extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
