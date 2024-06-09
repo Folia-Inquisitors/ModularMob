fce427947e9cdf8e6dd99c3c0542462d4b063e49
hsgamer <huynhqtienvtag@gmail.com>
2024-06-09 23:02:03 +0700

    add MobManager

--- src/main/java/me/hsgamer/modularmob/ModularMob.java
+++ src/main/java/me/hsgamer/modularmob/ModularMob.java
@@ -3,6 +3,7 @@ package me.hsgamer.modularmob;
 import io.github.projectunified.minelib.plugin.base.BasePlugin;
 import me.hsgamer.modularmob.builder.MobFactoryBuilder;
 import me.hsgamer.modularmob.builder.MobModifierBuilder;
+import me.hsgamer.modularmob.manager.MobManager;
 
 import java.util.Arrays;
 import java.util.List;
@@ -12,7 +13,9 @@ public final class ModularMob extends BasePlugin {
     protected List<Object> getComponents() {
         return Arrays.asList(
                 new MobFactoryBuilder(this),
-                new MobModifierBuilder(this)
+                new MobModifierBuilder(this),
+
+                new MobManager(this)
         );
     }
 }
---
+++ src/main/java/me/hsgamer/modularmob/manager/MobManager.java
@@ -0,0 +1,50 @@
+package me.hsgamer.modularmob.manager;
+
+import io.github.projectunified.minelib.plugin.base.Loadable;
+import me.hsgamer.hscore.bukkit.config.BukkitConfig;
+import me.hsgamer.modularmob.ModularMob;
+import me.hsgamer.modularmob.api.MobFactory;
+import me.hsgamer.modularmob.builder.MobFactoryBuilder;
+
+import java.io.File;
+import java.util.*;
+
+public class MobManager implements Loadable {
+    private final ModularMob plugin;
+    private final Map<String, MobFactory> mobFactoryMap = new HashMap<>();
+
+    public MobManager(ModularMob plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public void enable() {
+        File mobFolder = new File(plugin.getDataFolder(), "mobs");
+        if (!mobFolder.exists()) {
+            mobFolder.mkdirs();
+        }
+
+        MobFactoryBuilder mobFactoryBuilder = plugin.get(MobFactoryBuilder.class);
+        for (File file : Objects.requireNonNull(mobFolder.listFiles())) {
+            if (file.getName().endsWith(".yml") || file.getName().endsWith(".yaml")) {
+                BukkitConfig config = new BukkitConfig(file);
+                config.setup();
+                String name = config.getName();
+                mobFactoryBuilder.build(new MobFactoryBuilder.Input(name, config)).ifPresent(mobFactory -> mobFactoryMap.put(name, mobFactory));
+            }
+        }
+    }
+
+    @Override
+    public void disable() {
+        mobFactoryMap.clear();
+    }
+
+    public Optional<MobFactory> getMobFactory(String name) {
+        return Optional.ofNullable(mobFactoryMap.get(name));
+    }
+
+    public Map<String, MobFactory> getMobFactoryMap() {
+        return Collections.unmodifiableMap(mobFactoryMap);
+    }
+}
