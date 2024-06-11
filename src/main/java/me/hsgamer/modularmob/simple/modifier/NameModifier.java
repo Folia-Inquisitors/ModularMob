67e23d28b489d6d80004dfabf5cdc5b48afae0a8
hsgamer <huynhqtienvtag@gmail.com>
2024-06-12 00:50:59 +0700

    colorize name

--- src/main/java/me/hsgamer/modularmob/simple/modifier/NameModifier.java
+++ src/main/java/me/hsgamer/modularmob/simple/modifier/NameModifier.java
@@ -1,13 +1,16 @@
 package me.hsgamer.modularmob.simple.modifier;
 
+import me.hsgamer.hscore.bukkit.utils.ColorUtils;
 import me.hsgamer.modularmob.api.MobModifier;
 import org.bukkit.entity.Entity;
+
+import java.util.Objects;
 
 public class NameModifier implements MobModifier {
     private final String name;
 
     public NameModifier(Object value) {
-        this.name = String.valueOf(value);
+        this.name = ColorUtils.colorize(Objects.toString(value));
     }
 
     @Override
