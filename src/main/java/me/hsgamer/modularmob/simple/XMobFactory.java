05b5df1a72d19d0f8126b59e2c0a73b0ef3224d0
hsgamer <huynhqtienvtag@gmail.com>
2024-06-12 01:31:19 +0700

    correct entity path in XMobFactory

--- src/main/java/me/hsgamer/modularmob/simple/XMobFactory.java
+++ src/main/java/me/hsgamer/modularmob/simple/XMobFactory.java
@@ -1,21 +1,37 @@
 package me.hsgamer.modularmob.simple;
 
 import com.cryptomorin.xseries.XEntity;
+import me.hsgamer.hscore.config.PathString;
 import me.hsgamer.modularmob.api.MobFactory;
 import me.hsgamer.modularmob.builder.MobFactoryBuilder;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.MemoryConfiguration;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 
+import java.util.Locale;
 import java.util.Map;
 import java.util.Optional;
 
 public class XMobFactory implements MobFactory {
+    private final EntityType entityType;
     private final ConfigurationSection section;
 
     public XMobFactory(MobFactoryBuilder.Input input) {
         section = mapToConfigSection(input.config.getNormalizedValues(false));
+
+        EntityType entityType = null;
+        String entityTypeStr = input.config.getInstance(new PathString("entity"), String.class);
+        if (entityTypeStr != null) {
+            try {
+                entityType = EntityType.valueOf(entityTypeStr.toUpperCase(Locale.ROOT).trim());
+            } catch (IllegalArgumentException ignored) {
+                // IGNORED
+            }
+        }
+        this.entityType = entityType;
     }
 
     // Copy from [XSeries](https://github.com/CryptoMorin/XSeries/blob/82ca645e8edb4b94e42b0173637e6a178bea1f4b/src/main/java/com/cryptomorin/xseries/XItemStack.java#L1079-L1094)
@@ -39,7 +55,18 @@ public class XMobFactory implements MobFactory {
 
     @Override
     public Optional<Entity> spawn(Location location) {
-        return Optional.ofNullable(XEntity.spawn(location, section));
+        if (entityType == null) {
+            return Optional.empty();
+        }
+
+        World world = location.getWorld();
+        if (world == null) {
+            return Optional.empty();
+        }
+
+        Entity entity = world.spawnEntity(location, entityType);
+        XEntity.edit(entity, section);
+        return Optional.of(entity);
     }
 
     @Override
